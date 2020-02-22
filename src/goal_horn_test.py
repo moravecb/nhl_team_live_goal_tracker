114fa59fa069b26899ee3fbd77866893ae4f20c6
ptball <peytontball@gmail.com>
2020-02-21 19:31:05 -0600

    Adding lights to goal horn test

--- src/goal_horn_test.py
+++ src/goal_horn_test.py
@@ -19,6 +19,7 @@ team_name = args.team
 goal_horn = goal_horn_selector.get_goal_horn_file(team_name)
 
 try:
-    os.system('afplay "{}" &> /dev/null'.format(goal_horn)) # Plays goal horn
+    #os.system('afplay "{}" &> /dev/null'.format(goal_horn)) # Plays goal horn
+    os.system('afplay "{}" &> /dev/null & python3 lights_api_handler.py -t {}'.format(goal_horn, team_name))
 except:
     print("ERROR: Goalhorn .mp3 file couldn't be found.")
